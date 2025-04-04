778f041d801378f8e722761e6a0a0565510dc691
Sourave Halder <souravh093@gmail.com>
2025-04-04 12:42:10 +0600

    [Setup]: wit eslint, prettierr, typescript, express.js, cors, dotenv and more

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import express, { Application } from 'express';
+import cors from 'cors';
+import config from './app/config';
+
+// Initialize the express application
+const app: Application = express();
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+app.use(cors());
+
+// Middleware to handle CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  next();
+});
+
+// Middleware to log requests
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+// Home route
+app.get('/', (req, res) => {
+  res.json({
+    success: true,
+    message: 'Welcome to the Backend in Onneshon!',
+  });
+});
+
+// Running the server
+app.listen(config.PORT, () => {
+  console.log(`Server is running on port ${config.PORT}`);
+});
